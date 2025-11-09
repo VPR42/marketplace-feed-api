6ba2dd88287fe875cf3d7c4f586634f27960fc0e
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-11-09 18:27:33 +0300

    Включил поддержку транзакций

--- src/main/java/com/vpr42/marketplacefeedapi/MarketplaceFeedApiApplication.java
+++ src/main/java/com/vpr42/marketplacefeedapi/MarketplaceFeedApiApplication.java
@@ -2,8 +2,10 @@ package com.vpr42.marketplacefeedapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class MarketplaceFeedApiApplication {
 
     public static void main(String[] args) {
