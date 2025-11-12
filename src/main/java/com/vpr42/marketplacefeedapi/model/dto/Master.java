b38d14d97fdc01d0f7d691d7eb2733c79b48f59b
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-11-12 17:09:32 +0300

    Основные Dto проекта

--- src/main/java/com/vpr42/marketplacefeedapi/model/dto/Master.java
+++ src/main/java/com/vpr42/marketplacefeedapi/model/dto/Master.java
@@ -1,12 +1,15 @@
 package com.vpr42.marketplacefeedapi.model.dto;
 
-import java.util.List;
+import java.util.Set;
 
+/**
+ * Dto с информацией о мастере
+ */
 public record Master(
     Integer experience,
     String description,
     String pseudonym,
     String phoneNumber,
     String workingHours,
-    List<String> skills
+    Set<String> skills
 ) { }
---
+++ src/main/java/com/vpr42/marketplacefeedapi/model/dto/Service.java
@@ -0,0 +1,23 @@
+package com.vpr42.marketplacefeedapi.model.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Dto с информацией об услуге
+ */
+public record Service(
+        UUID id,
+        String name,
+        String description,
+        BigDecimal price,
+        String coverUrl,
+        LocalDateTime createdAt,
+        User master,
+        Category category,
+        Set<String> tags,
+        Integer ordersCount
+) {
+}
