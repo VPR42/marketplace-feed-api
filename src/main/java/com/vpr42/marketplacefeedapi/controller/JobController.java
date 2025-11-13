1155521d4e5e9504f2ad5820849f9f03d45deb73
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-11-13 21:37:52 +0300

    поправил эндпоинт и вернул ResponseEntity

--- src/main/java/com/vpr42/marketplacefeedapi/controller/JobController.java
+++ src/main/java/com/vpr42/marketplacefeedapi/controller/JobController.java
@@ -7,13 +7,14 @@ import com.vpr42.marketplacefeedapi.service.JobService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/jobs")
+@RequestMapping("/api/feed/jobs")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -21,9 +22,11 @@ public class JobController {
     private final JobService jobService;
 
     @PostMapping
-    public Job createJob(@RequestBody @Valid CreateJobDto dto,
-                                         @AuthenticationPrincipal UserEntity user) {
+    public ResponseEntity<Job> createJob(@RequestBody @Valid CreateJobDto dto,
+                                       @AuthenticationPrincipal UserEntity user) {
         log.info("Processing new create job request from user: {}", user.getId());
-        return jobService.createJob(dto, user);
+        Job result = jobService.createJob(dto, user);
+
+        return ResponseEntity.ok(result);
     }
 }
