7bc234b0bfaafc800fb462e086c69b5b9bf321be
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-11-19 01:39:49 +0300

    VJ-26: Реализовать получение списка услуг по фильтрам
    Перенес фильтры + добавил валидацию

--- src/main/java/com/vpr42/marketplacefeedapi/controller/JobController.java
+++ src/main/java/com/vpr42/marketplacefeedapi/controller/JobController.java
@@ -2,16 +2,21 @@ package com.vpr42.marketplacefeedapi.controller;
 
 import com.vpr42.marketplacefeedapi.model.dto.CreateJobDto;
 import com.vpr42.marketplacefeedapi.model.dto.Job;
+import com.vpr42.marketplacefeedapi.model.dto.JobFilters;
 import com.vpr42.marketplacefeedapi.model.entity.UserEntity;
 import com.vpr42.marketplacefeedapi.service.JobService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/feed/jobs")
@@ -28,5 +33,14 @@ public class JobController {
         Job result = jobService.createJob(dto, user);
 
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Job>> getJobs(
+        @ModelAttribute @Valid JobFilters filters,
+        @AuthenticationPrincipal UserEntity user
+    ) {
+        return ResponseEntity
+                .ok(null);
     }
 }
