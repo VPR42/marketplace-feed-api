a96da29a0373764f43635f00cbf2fc66f600685b
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-11-21 21:55:26 +0300

    VJ-29: Изменил контракт взаимодействия

--- src/main/java/com/vpr42/marketplacefeedapi/controller/CitiesController.java
+++ src/main/java/com/vpr42/marketplacefeedapi/controller/CitiesController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +17,18 @@ public class CitiesController {
     private final CityService cityService;
 
     @GetMapping
-    public ResponseEntity<Page<City>> getCities() {
+    public ResponseEntity<Page<City>> getCities(
+        @RequestParam(value = "withOrders", required = false, defaultValue = "false") boolean withOrders,
+        @RequestParam(value = "query", required = false) String query,
+        @RequestParam(value = "orderedByOrdersCount", required = false, defaultValue = "false") boolean orderedByOrdersCount,
+        @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+        @RequestParam(value = "pageSize", required = false, defaultValue = "15") int pageSize
+    ) {
         return ResponseEntity
-                .ok(cityService.getCities());
+                .ok(cityService.getCities(query,
+                        withOrders,
+                        orderedByOrdersCount,
+                        page,
+                        pageSize));
     }
 }
