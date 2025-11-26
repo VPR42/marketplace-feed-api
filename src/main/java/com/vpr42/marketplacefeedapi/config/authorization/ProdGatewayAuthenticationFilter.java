624aaf9aeb9b423eeeb45a018e5bda26aa2e98ab
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-11-26 22:52:58 +0300

    VJ-61: Конфиг безопасности для использования со шлюзом

---
+++ src/main/java/com/vpr42/marketplacefeedapi/config/authorization/ProdGatewayAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.vpr42.marketplacefeedapi.config.authorization;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ProdGatewayAuthenticationFilter extends OncePerRequestFilter {
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
+        log.info("Processing new user request");
+
+        String idValue = request.getHeader("id");
+        String emailValue = request.getHeader("id");
+        if (idValue == null || emailValue == null
+                || idValue.isBlank() || emailValue.isBlank()) {
+            log.info("User doesn't have credentials info from gateway");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            UUID id = UUID.fromString(idValue);
+            Authentication authentication = new FromHeaderAuthorizationToken(id, emailValue);
+            this.authenticationManager.authenticate(authentication);
+        } catch (IllegalArgumentException exception) {
+            log.error("Invalid credentials. Id can't be null");
+        } catch (Exception ex) {
+            log.error("Failed to authenticate user. Reason", ex);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/vpr42/marketplacefeedapi/config/authorization/SecurityConfig.java
+++ src/main/java/com/vpr42/marketplacefeedapi/config/authorization/SecurityConfig.java
@@ -8,26 +8,40 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 /**
- * Настройка конфигурации безопасности, сейчас просто разрешает любой запрос
+ * Настройка конфигурации безопасности
  */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
+    @Profile("prod")
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) throws Exception {
+        FromHeaderAuthenticationProvider provider = new FromHeaderAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider::authenticate;
     }
 
     @Bean
     @Profile("prod")
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, ProdGatewayAuthenticationFilter filter)
+            throws Exception {
         http
-            .authorizeHttpRequests(req -> req.anyRequest().permitAll())
-            .httpBasic(AbstractHttpConfigurer::disable);
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(req ->
+                req
+                    .requestMatchers("/api/feed/docs").permitAll()
+                    .requestMatchers("api/feed/**").authenticated()
+            )
+            .addFilterAfter(filter, LogoutFilter.class);
 
         return http.build();
     }
