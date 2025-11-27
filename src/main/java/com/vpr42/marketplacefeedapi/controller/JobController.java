183c30d9c9a86f222c57c8451910100ca34c81b2
Запара Иван <truthpruthbg@gmail.com>
2025-11-27 23:24:18 +0300

    VJ-84: Импорты убрал звездочка

--- src/main/java/com/vpr42/marketplacefeedapi/controller/JobController.java
+++ src/main/java/com/vpr42/marketplacefeedapi/controller/JobController.java
@@ -1,6 +1,10 @@
 package com.vpr42.marketplacefeedapi.controller;
 
-import com.vpr42.marketplacefeedapi.model.dto.*;
+import com.vpr42.marketplacefeedapi.model.dto.ApiErrorResponse;
+import com.vpr42.marketplacefeedapi.model.dto.CreateJobDto;
+import com.vpr42.marketplacefeedapi.model.dto.Job;
+import com.vpr42.marketplacefeedapi.model.dto.JobFilters;
+import com.vpr42.marketplacefeedapi.model.dto.UpdateJobDto;
 import com.vpr42.marketplacefeedapi.model.entity.UserEntity;
 import com.vpr42.marketplacefeedapi.service.JobService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,7 +19,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
